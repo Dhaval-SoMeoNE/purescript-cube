92e6f501040941919d965a21ed0c61dc8f30d8c9
Dhaval-SoMeoNE <dhavalkhatri1996@gmail.com>
2017-08-10 22:12:32 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,12 @@
 
 "use strict"
 var speed = 0.0
-var flag1 = false
-
 
 exports.changeSpeed = function(speed1) {
 if (speed1 < 20.0 )
     speed = 0.0;
-else if ( speed1 > 400.0)
-    speed = 200.0;
+else if ( speed1 > 1000.0)
+    speed = 1000.0;
 else 
     speed = speed1;
 
@@ -16,11 +14,6 @@ console.log(speed,"changeSpeed");
 return speed1;
 };
 
-exports.flag = function (temp)
-{
-    flag1 = ! flag1;
-    return flag1;
-}
 exports.getSpeed = function(dumy)
 {
     console.log(speed,"getSpeed");
