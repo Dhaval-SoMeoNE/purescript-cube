ccd700b1ab805f28b4077b495efb0f236160a894
Dhaval-SoMeoNE <dhavalkhatri1996@gmail.com>
2017-08-10 23:48:05 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,32 @@
 
 "use strict"
 var speed = 0.0
+var axis = 'x'
+var direction = 1.0
 
+exports.changeDirection= function(diff)
+{
+if (diff<0)
+    direction = -1.0;
+else
+    direction = 1.0;
+return direction ;
+};
+exports.getDirection= function(ch)
+{
+    return direction;
+};
+exports.changeAxis = function(ch)
+{
+    axis = ch;
+    console.log("Change Axis =", axis);
+    return axis;
+};
+exports.getAxis=function (ch)
+{
+    console.log("get Axis =", axis);
+  return axis;
+};
 exports.changeSpeed = function(speed1) {
 if (speed1 < 20.0 )
     speed = 0.0;
